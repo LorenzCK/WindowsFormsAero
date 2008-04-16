81a46bd73ee97c5273d0456c1a86e4d1034acd9f
jachymko_cp <julie@juliekoubova.net>
2008-04-16 22:55:45 +0000

    AeroTabControl keyboard navigation enhancements (Ctrl+1 to 9, Ctrl+W, Ctrl+T), plus fully configurable

--- WindowsFormsAero/WindowsFormsAeroDemo/Form1.cs
+++ WindowsFormsAero/WindowsFormsAeroDemo/Form1.cs
@@ -15,6 +15,16 @@ namespace WindowsFormsAero.Demo
             InitializeComponent();
         }
 
+        private void aeroTabControl1_NewTabButtonClick(object sender, EventArgs e)
+        {
+            aeroTabControl1.TabPages.Add(new AeroTabPage());
+        }
+
+        private void aeroTabControl1_CloseButtonClick(object sender, AeroTabPageEventArgs e)
+        {
+            aeroTabControl1.TabPages.Remove(e.Page);
+        }
+
         //protected override void OnLoad(EventArgs e)
         //{
         //    base.OnLoad(e);
