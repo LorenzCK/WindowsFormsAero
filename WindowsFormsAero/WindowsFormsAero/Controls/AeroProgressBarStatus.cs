98897180afc2acb5c1604f7870968974531b454e
jachymko_cp <julie@juliekoubova.net>
2008-04-17 22:40:37 +0000

    AeroProgressBar

---
+++ WindowsFormsAero/WindowsFormsAero/Controls/AeroProgressBarStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormsAero
+{
+    [Serializable]
+    public enum AeroProgressBarStatus
+    {
+        Normal = 1,
+        Failed = 2,
+        Paused = 3,
+    }
+}
---
+++ WindowsFormsAero/WindowsFormsAero/InteropServices/WindowStyles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormsAero.InteropServices
+{
+    internal sealed class WindowStyles
+    {
+        public const int PBS_SMOOTHREVERSE = 0x10;
+    }
+}
