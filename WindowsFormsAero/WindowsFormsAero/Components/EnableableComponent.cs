e533af3c75ed68e997570789dac297e067aefab7
jachymko_cp <SND\jachymko_cp>
2008-05-04 15:00:04 +0000

    very very broken keyboard hook. need to rethink the whole thing

---
+++ WindowsFormsAero/WindowsFormsAero/Components/EnableableComponent.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace WindowsFormsAero
+{
+    [System.ComponentModel.DefaultProperty("Enabled")]
+    [System.ComponentModel.DesignerCategory("Code")]
+    public abstract class EnableableComponent : Component
+    {
+        private Boolean _enabled;
+
+        [DefaultValue(false)]
+        [Browsable(true)]
+        public bool Enabled
+        {
+            get { return _enabled; }
+            set
+            {
+                if (_enabled != value)
+                {
+                    _enabled = value;
+
+                    if (!DesignMode && value)
+                    {
+                        OnStart();
+                    }
+                    else if (!DesignMode && !value)
+                    {
+                        OnStop();
+                    }
+                }
+            }
+        }
+
+        public void Start()
+        {
+            Enabled = true;
+        }
+
+        public void Stop()
+        {
+            Enabled = false;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Stop();
+            }
+            
+            base.Dispose(disposing);
+        }
+
+        protected abstract void OnStart();
+
+        protected abstract void OnStop();
+    }
+}
---
+++ WindowsFormsAero/WindowsFormsAero/Components/Hooks/HookWindow.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using WindowsFormsAero.InteropServices;
+
+namespace WindowsFormsAero
+{
+    internal sealed partial class HookWindow
+    {
+        private readonly EventHandlerList Events = new EventHandlerList();
+        private readonly Object SyncRoot = new Object();
+        
+        private readonly HookNativeWindow _window;
+        private readonly HookLibrary _library;
+
+        public event KeyEventHandler KeyDown
+        {
+            add 
+            {
+                lock (SyncRoot)
+                {
+                    Events.AddHandler(EventKeyDown, value);
+                    UpdateKeyboardHook();
+                }
+            }
+            remove 
+            {
+                lock (SyncRoot)
+                {
+                    Events.RemoveHandler(EventKeyDown, value);
+                    UpdateKeyboardHook();
+                }
+            }
+        }
+
+        private HookWindow()
+        {
+            const int HWND_MESSAGE = -3;
+
+            _window = new HookNativeWindow(this);
+            _window.CreateHandle(new CreateParams()
+            {
+                Parent = new IntPtr(HWND_MESSAGE),
+            });
+
+            _library = new HookLibrary(_window.Handle);
+        }
+
+        private void Dispose()
+        {
+            _library.Dispose();
+            _window.DestroyHandle();
+        }
+
+        private void UpdateKeyboardHook()
+        {
+            _library.SetHook(WindowsHookType.Keyboard, HasKeyboardEventHandlers);
+        }
+
+        private bool HasKeyboardEventHandlers
+        {
+            get
+            {
+                return Events[EventKeyDown] != null;
+            }
+        }
+
+        private static readonly object EventKeyDown = new object();
+
+        private sealed class HookNativeWindow : NativeWindow
+        {
+            private readonly HookWindow _owner;
+
+            public HookNativeWindow(HookWindow owner)
+            {
+                _owner = owner;
+            }
+            protected override void WndProc(ref Message m)
+            {
+                switch (m.Msg)
+                {
+                    case WindowMessages.WM_KEYDOWN:
+                        System.Diagnostics.Debug.WriteLine("WM_KEYDOWN");
+                        break;
+
+                    case WindowMessages.WM_KEYUP:
+                        System.Diagnostics.Debug.WriteLine("WM_KEYUP");
+                        break;
+
+                    default:
+                        base.WndProc(ref m);
+                        break;
+                }
+            }
+        }
+    }
+}
