71f3fa03b0ed6c3ed07d96ec925468f6188979f0
jachymko_cp <SND\jachymko_cp>
2008-05-08 13:09:43 +0000

    DropShadowForm

---
+++ WindowsFormsAero/WindowsFormsAero/Forms/DropShadowForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Permissions;
+using System.Windows.Forms;
+
+namespace WindowsFormsAero
+{
+    [System.ComponentModel.DesignerCategory("Code")]
+    public class DropShadowForm : Form
+    {
+        private const int CS_DROPSHADOW = 0x00020000;
+
+        protected override CreateParams CreateParams
+        {
+            [SecurityPermission(SecurityAction.LinkDemand, UnmanagedCode = true)]
+            get
+            {
+                var parameters = base.CreateParams;
+
+                if (VistaOSFeature.Feature.IsPresent(VistaOSFeature.DropShadow))
+                {
+                    parameters.ClassStyle |= CS_DROPSHADOW;
+                }
+
+                return parameters;
+            }
+        }
+    }
+}
