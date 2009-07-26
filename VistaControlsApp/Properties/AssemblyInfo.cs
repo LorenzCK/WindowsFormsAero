aac86ffb6f831617cb7d87dad01bd65ac12e7ee5
bpell_cp <SND\bpell_cp>
2009-07-26 18:50:49 +0000

    - Updated project version information for 1.4 release.

--- VistaControlsApp/Properties/AssemblyInfo.cs
+++ VistaControlsApp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Not applicable")]
 [assembly: AssemblyProduct("VistaControlsApp")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
